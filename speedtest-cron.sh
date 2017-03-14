7cf56c75ed230eba9ee0d645a969c865e54cfe2a
Olivier De Decker <olivier.dedecker@gmail.com>
2017-03-14 12:08:59 +0100

    Création

---
+++ speedtest-cron.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ 
+cd /home/pi/speedtest-cli-extras/bin
+./speedtest-csv >> /home/pi/speedtest.log
---
+++ speedtest-csv.bat
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+###########################################################################
+# Copyright (C) 2014 Henrik Bengtsson
+# License: GPL (>= 2.1) [http://www.gnu.org/licenses/gpl.html]
+###########################################################################
+
+# Character for separating values
+# (commas are not safe, because some servers return speeds with commas)
+sep=";"
+
+# Temporary file holding speedtest-cli output
+user=$USER
+if test -z $user; then
+  user=$USERNAME
+fi
+log=/tmp/$user/speedtest-csv.log
+
+# Local functions
+function str_extract() {
+ pattern=$1
+ # Extract
+ res=`grep "$pattern" $log | sed "s/$pattern//g"`
+ # Drop trailing ...
+ res=`echo $res | sed 's/[.][.][.]//g'`
+ # Trim
+ res=`echo $res | sed 's/^ *//g' | sed 's/ *$//g'`
+ echo $res
+}
+
+# Display header?
+if test "$1" = "--header"; then
+  start="start"
+  stop="stop"
+  from="from"
+  from_ip="from_ip"
+  server="server"
+  server_dist="server_dist"
+  server_ping="server_ping"
+  download="download"
+  upload="upload"
+  share_url="share_url"
+else
+  mkdir -p `dirname $log`
+
+  start=`date +"%Y-%m-%d %H:%M:%S"`
+
+  if test -n "$SPEEDTEST_CSV_SKIP" && test -f "$log"; then
+    # Reuse existing results (useful for debugging)
+    1>&2 echo "** Reusing existing results: $log"
+  else
+    # Query Speedtest
+    speedtest-cli --share > $log
+  fi
+  
+  stop=`date +"%Y-%m-%d %H:%M:%S"`
+  
+  # Parse
+  from=`str_extract "Testing from "`
+  from_ip=`echo $from | sed 's/.*(//g' | sed 's/).*//g'`
+  from=`echo $from | sed 's/ (.*//g'`
+  
+  server=`str_extract "Hosted by "`
+  server_ping=`echo $server | sed 's/.*: //g'`
+  server=`echo $server | sed 's/: .*//g'`
+  server_dist=`echo $server | sed 's/.*\\[//g' | sed 's/\\].*//g'`
+  server=`echo $server | sed 's/ \\[.*//g'`
+  
+  download=`str_extract "Download: "`
+  upload=`str_extract "Upload: "`
+  share_url=`str_extract "Share results: "`
+fi
+
+# Standardize units?
+if test "$1" = "--standardize"; then
+  download=`echo $download | sed 's/Mbits/Mbit/'`
+  upload=`echo $upload | sed 's/Mbits/Mbit/'`
+fi
+  
+# Output CSV results
+echo $USER
+echo $USERNAME
+echo $start$sep$stop$sep$from$sep$from_ip$sep$server$sep$server_dist$sep$server_ping$sep$download$sep$upload$sep$share_url
+
+# Send to IFTTT
+secret_key="c5IVXseYVm-Z-2Ub-Ig9bMlBEceYebA8AdR3lPWl832"
+
+value1=`echo $server_ping | cut -d" " -f1`
+value2=`echo $download | cut -d" " -f1`
+value3=`echo $upload | cut -d" " -f1` 
+json="{\"value1\":\"${value1}\",\"value2\":\"${value2}\",\"value3\":\"${value3}\"}"
+curl -X POST -H "Content-Type: application/json" -d "${json}" https://maker.ifttt.com/trigger/speedtest/with/key/${secret_key}
